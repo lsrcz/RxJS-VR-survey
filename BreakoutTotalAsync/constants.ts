9e2a73f5ff6dab4321c8cbd1a3eddf0a4f18d4b8
Sirui Lu <lsrcz@pku.edu.cn>
2019-07-15 13:10:18 -0700

    total async

---
+++ BreakoutTotalAsync/constants.ts
@@ -0,0 +1,16 @@
+export const PADDLE_KEYS = {
+    left: 37,
+    right: 39
+};
+
+export const TICKER_INTERVAL = 17;
+
+export const BRICK_ROWS = 1;
+export const BRICK_COLUMNS = 7;
+export const BRICK_HEIGHT = 20;
+export const BRICK_GAP = 3;
+
+export const PADDLE_WIDTH = 100;
+export const PADDLE_HEIGHT = 20;
+
+export const BALL_RADIUS = 10;
